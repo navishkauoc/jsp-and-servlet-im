04a1142ab7201f7b94e55e0340edf2a2574352c5
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-01 13:57:09 +0530

    WebServlet() annotation is working

--- TestWorkspace/src/com/navishkakularathna/testworkshop/MyServlet.java
+++ TestWorkspace/src/com/navishkakularathna/testworkshop/MyServlet.java
@@ -1,14 +1,31 @@
 package com.navishkakularathna.testworkshop;
 
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		String name = "Navishka Kularathna";
+		Date today = new Date();
 		
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("<h3> Name : " + name + "</h3>");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		System.out.println("Servlet is working");
 	}
 	
 
