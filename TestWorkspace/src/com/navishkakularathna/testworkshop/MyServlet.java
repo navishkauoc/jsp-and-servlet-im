c252f57efbd0ed9be23d950c9dc01e09ee48c577
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-01 12:54:57 +0530

    doGet() method overrided

--- TestWorkspace/src/com/navishkakularathna/testworkshop/MyServlet.java
+++ TestWorkspace/src/com/navishkakularathna/testworkshop/MyServlet.java
@@ -1,7 +1,15 @@
 package com.navishkakularathna.testworkshop;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MyServlet extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+	
 
 }
