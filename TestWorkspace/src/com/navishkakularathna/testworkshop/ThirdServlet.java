eeee83fd98327551c9c82685d1ab42c4c18e84cb
Navishka Kularathna <navishka.randika@gmail.com>
2019-11-02 20:55:10 +0530

    mysql-connector-java library added

--- TestWorkspace/src/com/navishkakularathna/testworkshop/ThirdServlet.java
+++ TestWorkspace/src/com/navishkakularathna/testworkshop/ThirdServlet.java
@@ -3,14 +3,16 @@ package com.navishkakularathna.testworkshop;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/index.html")
 public class ThirdServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp){
-		
+		System.out.println("Third working");
 	}
 
 }
